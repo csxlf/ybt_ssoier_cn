8492134304d8da0425c3c60c76262810f2141296
csxlf <51782838+csxlf@users.noreply.github.com>
2021-04-18 02:09:53 +0800

    Create 1042.cpp

---
+++ 1042.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+int main() {
+    char a;
+    cin >> a;
+    int b=(int)a;
+    if(b%2==0){
+        cout <<"NO"<< endl;
+    }else{
+        cout <<"YES"<< endl;
+    }
+    return 0;
+}
