211d05f0ebb597592ce22a311b689a533134c8ec
csxlf <51782838+csxlf@users.noreply.github.com>
2021-04-20 12:20:04 +0800

    Create 1124.cpp

---
+++ 1124.cpp
@@ -0,0 +1,26 @@
+#include <cstdio>
+using namespace std;
+
+int main(){
+    int n,m,i,j,h=0;
+    scanf("%d %d",&n,&m);
+    int a[n][m],b[n][m];
+    for(i=0;i<n;i++){
+        for(j=0;j<m;j++){
+            scanf("%d",&a[i][j]);
+        }
+    }
+    for(i=0;i<n;i++){
+        for(j=0;j<m;j++){
+            scanf("%d",&b[i][j]);
+        }
+    }
+    for(i=0;i<n;i++){
+        for(j=0;j<m;j++){
+            h=a[i][j]+b[i][j];
+            printf("%d ",h);
+        }
+        11printf("\n");
+    }
+    return 0;
+}
